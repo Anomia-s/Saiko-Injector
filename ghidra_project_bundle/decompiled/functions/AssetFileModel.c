4890c6eef530c60c22e308845a96d738198156bb
Bluefield <48862659+bluefield-creator@users.noreply.github.com>
2026-07-07 03:49:19 +0200

    Refactor AssetFileModel constructor for clarity

--- ghidra_project_bundle/decompiled/functions/AssetFileModel.c
+++ ghidra_project_bundle/decompiled/functions/AssetFileModel.c
@@ -4,25 +4,23 @@
  * Signature: AssetFileModel * __thiscall AssetFileModel(AssetFileModel * this, QObject * param_1)
  * Body size: 128 bytes
  */
-
-
-AssetFileModel * __thiscall
-ldv::editor::AssetFileModel::AssetFileModel(AssetFileModel *this,QObject *param_1)
-
+ldv::editor::AssetFileModel::AssetFileModel(QObject *parent)
+    : QAbstractListModel(parent)
 {
-  undefined1 *this_00;
-  
-  QAbstractListModel::QAbstractListModel((QAbstractListModel *)this,param_1);
-  this->vftablePtr = &vftable;
-  QString::QString((QString *)&(this->AssetFileModel_data).field_0x8);
-  this_00 = &(this->AssetFileModel_data).field_0x40;
-  (this->AssetFileModel_data).offset_0x20 = 0;
-  (this->AssetFileModel_data).offset_0x28 = 0;
-  (this->AssetFileModel_data).offset_0x30 = 0;
-  (this->AssetFileModel_data).offset_0x38 = 0;
-  QCollator::QCollator((QCollator *)this_00);
-  QCollator::setCaseSensitivity((QCollator *)this_00,0);
-  QCollator::setNumericMode((QCollator *)this_00,true);
-  return this;
-}
+    // vtable assignment is compiler/decompiler noise.
 
+    new (&this->AssetFileModel_data.field_0x8) QString();
+
+    this->AssetFileModel_data.offset_0x20 = nullptr;
+    this->AssetFileModel_data.offset_0x28 = nullptr;
+    this->AssetFileModel_data.offset_0x30 = nullptr;
+    this->AssetFileModel_data.offset_0x38 = nullptr;
+
+    new (&this->AssetFileModel_data.field_0x40) QCollator();
+
+    auto *collator =
+        reinterpret_cast<QCollator *>(&this->AssetFileModel_data.field_0x40);
+
+    collator->setCaseSensitivity(Qt::CaseInsensitive);
+    collator->setNumericMode(true);
+}
